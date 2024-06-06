53a7dc7e3aabbc235fffcef4476bf418ebe3fff7
sasi-kanakinti <sasi-kanakinti@users.noreply.github.com>
2024-06-06 15:47:39 +0530

    Initial commit

---
+++ p3/src/main/java/fsj/p3/App.java
@@ -0,0 +1,13 @@
+package fsj.p3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+
+    }
+}
